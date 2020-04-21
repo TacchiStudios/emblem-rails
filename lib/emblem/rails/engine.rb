acf3c3432e74001ef01f7aec60a20510c37b1c9c
Adam Akhtar <adamsubscribe@googlemail.com>
2020-04-21 10:40:00 +0900

    new: update registration of handler to work with sprockets 2, 3 & 4
    This project is no longer maintained. Currently it will only work with
    Sprockets v2. However other gems, especially Rails 5 and 6 will only
    work with later versions of Sprockets (3 & 4).
    
    This commit adjusts the registration of the handler to work with
    later sprocket versions.
    
    See:
    https://github.com/rails/sprockets/blob/9909da64595ddcfa1e7ee40ed1d99738961288ec/guides/extending_sprockets.md
    https://github.com/prezentt/emblem-rails/commit/a2630f02f195a8d0cebff92ea11f4fb3dfe3aa2b

--- lib/emblem/rails/engine.rb
+++ lib/emblem/rails/engine.rb
@@ -4,7 +4,16 @@ module Emblem
   module Rails
     class Engine < ::Rails::Engine
       initializer "ember_rails.setup", :after => :append_assets_path, :group => :all do |app|
-        app.assets.register_engine '.emblem', Emblem::Rails::Template
+        app.config.assets.configure do |env|
+          if env.respond_to?(:register_transformer)
+            env.register_mime_type 'text/js', extensions: ['.js'], charset: :js
+            env.register_preprocessor 'text/emblem', Emblem::Rails::Template
+          elsif env.respond_to?(:register_engine)
+            args = ['.emblem', Emblem::Rails::Template]
+            args << { mime_type: 'text/js', silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+            env.register_engine(*args)
+          end
+        end
       end
     end
   end
